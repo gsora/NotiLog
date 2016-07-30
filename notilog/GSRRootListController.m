600f747028ca4655926aa6d0c74bcc0bd0416fee
Gianguido Sorà <g.sora4@gmail.com>
2016-07-30 22:30:32 +0200

    new preferences!
    now we post darwin notifications when something changes, instead of reloading the preferences when every switch is being called

---
+++ notilog/GSRRootListController.m
@@ -0,0 +1,13 @@
+#include "GSRRootListController.h"
+
+@implementation GSRRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
