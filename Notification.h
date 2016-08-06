3e9a63d5e6e467761ac5e9bef72108d2ec66cce3
Gianguido Sorà <g.sora4@gmail.com>
2016-08-06 03:41:25 +0200

    added date support

--- Notification.h
+++ Notification.h
@@ -1,15 +1,16 @@
 #import <Foundation/Foundation.h>
 
-@interface Notification : NSObject {}
+@interface Notification : NSObject <NSCoding> {}
 
 @property (nonatomic,retain) NSString *message;
 @property (nonatomic,retain) NSString *bulletinID;
 @property (nonatomic,retain) NSString *title;
 @property (nonatomic,retain) NSString *subtitle;
 @property (nonatomic,retain) NSString *section;
+@property (nonatomic, retain) NSString *date;
 
 - (id)init;
-- (id)initWithMessage:(NSString *)message bulletinID:(NSString *)bulletinID title:(NSString *)title subtitle:(NSString *)subtitle section:(NSString *)section; 
+- (id)initWithMessage:(NSString *)message bulletinID:(NSString *)bulletinID title:(NSString *)title subtitle:(NSString *)subtitle section:(NSString *)section date:(NSString *)date; 
 - (id)initWithCoder:(NSCoder *)aDecoder;
 - (void)encodeWithCoder:(NSCoder *)encoder;
 @end
--- Notification.m
+++ Notification.m
@@ -1,11 +1,12 @@
 #import "Notification.h"
 
 @implementation Notification
+
 - (id)init {
-	return [self initWithMessage: nil bulletinID:nil title:nil subtitle:nil section:nil];
+	return [self initWithMessage: nil bulletinID:nil title:nil subtitle:nil section:nil date:nil];
 }
 
-- (id)initWithMessage:(NSString *)message bulletinID:(NSString *)bulletinID title:(NSString *)title subtitle:(NSString *)subtitle section:(NSString *)section {
+- (id)initWithMessage:(NSString *)message bulletinID:(NSString *)bulletinID title:(NSString *)title subtitle:(NSString *)subtitle section:(NSString *)section date:(NSString *)date {
 	self = [super init];
 	if(self) {
 		_message = message;
@@ -13,7 +14,30 @@
 		_title = title;
 		_subtitle = subtitle;
 		_section = section;
+		_date = date;
 	}
 	return self;
 }
+
+- (void)encodeWithCoder:(NSCoder *)encoder {
+  [encoder encodeObject:_message forKey:@"message"];
+  [encoder encodeObject:_bulletinID forKey:@"bulletinID"];
+  [encoder encodeObject:_title forKey:@"title"];
+  [encoder encodeObject:_subtitle forKey:@"subtitle"];
+  [encoder encodeObject:_section forKey:@"section"];
+  [encoder encodeObject:_date forKey:@"date"];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	NSString *message = [aDecoder decodeObjectForKey:@"message"];
+	NSString *bulletinID = [aDecoder decodeObjectForKey:@"bulletinID"];
+	NSString *title = [aDecoder decodeObjectForKey:@"title"];
+	NSString *subtitle = [aDecoder decodeObjectForKey:@"subtitle"];
+	NSString *section = [aDecoder decodeObjectForKey:@"section"];
+	NSString *date = [aDecoder decodeObjectForKey:@"date"];
+	return [self initWithMessage:message bulletinID:bulletinID title:title subtitle:subtitle section:section date:date];
+}
+
+
+
 @end
